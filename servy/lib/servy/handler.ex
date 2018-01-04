981e18fe6e7eedb76c62aa539dae145301d9a7af
Raymond Maung <raymaung@gmail.com>
2018-01-03 19:43:05 -0700

    Another GenServer

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -31,16 +31,8 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
-    task = Task.async(Servy.Tracker, :get_location, ["bigfoot"])
-
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(VideoCam, :get_snapshot, [&1]))
-      |> Enum.map(&Task.await/1)
-
-    where_is_bigfoot = Task.await(task)
-
-    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
+    sensor_data = Servy.SensorServer.get_sensor_data()
+    %{ conv | status: 200, resp_body: inspect(sensor_data) }
   end
 
   def route(%Conv{ method: "GET", path: "/snapshots" } = conv) do
---
+++ servy/lib/servy/sensor_server.ex
@@ -0,0 +1,61 @@
+defmodule Servy.SensorServer do
+
+  @name :sensor_server
+
+  @refresh_interval :timer.seconds(5)
+
+  alias Servy.VideoCam
+
+  use GenServer
+
+  # Client Interface
+  def start do
+    GenServer.start(__MODULE__, %{}, name: @name)
+  end
+
+  def get_sensor_data do
+    GenServer.call @name,:get_sensor_data
+  end
+
+  # Server Callbacks
+
+  def init(_state) do
+    initial_state = run_tasks_to_get_sensor_data()
+    schedule_refresh()
+    {:ok, initial_state}
+  end
+
+  def handle_info(:refresh, _state) do
+    new_state = run_tasks_to_get_sensor_data()
+    schedule_refresh()
+    {:noreply, new_state}
+  end
+
+  def handle_info(unexpected, state) do
+    IO.puts "Can't touch this! #{inspect unexpected}"
+    {:noreply, state}
+  end
+
+  def handle_call(:get_sensor_data, _from, state) do
+    {:reply, state, state}
+  end
+
+  defp run_tasks_to_get_sensor_data do
+    IO.puts "Running tasks to get sensor data"
+    task = Task.async(Servy.Tracker, :get_location, ["bigfoot"])
+
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Task.async(VideoCam, :get_snapshot, [&1]))
+      |> Enum.map(&Task.await/1)
+
+    where_is_bigfoot = Task.await(task)
+
+    %{snapshots: snapshots, location: where_is_bigfoot}
+  end
+
+  defp schedule_refresh() do
+    Process.send_after(self(), :refresh, @refresh_interval)
+  end
+
+end
